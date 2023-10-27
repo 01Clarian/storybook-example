cc0f958710f9613a2540e1e60a6e6525f35cdaaa
01clarian <clarionnorth@gmail.com>
2023-10-27 10:02:16 -0500

    updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.@(js|jsx)'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/react-vite",
