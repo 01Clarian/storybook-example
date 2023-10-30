aa9dc3d9ff005ff06cad4ae218fcfb64e68bbfc4
01clarian <clarionnorth@gmail.com>
2023-10-30 10:13:43 -0500

    test storybook automated changes to chromatic

--- src/components/TaskList.stories.jsx
+++ src/components/TaskList.stories.jsx
@@ -14,6 +14,8 @@ export const MockedState = {
     { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
     { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: '7', title: 'Task 7' },
+
   ],
   status: 'idle',
   error: null,
---
+++ storybook-static/assets/preview-25f86b7e.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-c1111e86.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-ff5b7089.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-ff5b7089.js","./iframe-c1111e86.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-e1e43787.js","./index-d3ea75b5.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-25f86b7e.js.map
