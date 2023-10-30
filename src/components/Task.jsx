4e9820b5a84b3ca06413efeb1b847685c1c26a53
01clarian <clarionnorth@gmail.com>
2023-10-30 10:51:35 -0500

    change background color fix

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -28,7 +28,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
-+         style={{ background: 'red' }}
+          style={{ background: 'red' }}
         />
       </label>
 
